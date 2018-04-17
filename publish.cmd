a6382d629bbe94ab522087dfcbf04688c38711b3
Nag <nagarjuna.chirala@gmail.com>
2018-04-16 17:23:26 -0700

    initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=nags-bot-844f-Web-Deploy.pubxml -p:Password=zpxJjr2Msfe9WcN5hNsKxoQDmjqqDLgM7Ks6g8djofbch6FlrQ3E5xTwgbAJ
+
